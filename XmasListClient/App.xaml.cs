a38997a7b41c10a071dbc39a9b3e219c7526d092
Simon Lamb <silamb@microsoft.com>
2017-02-27 22:53:06 +1100

    Cleaned up default settings

--- XmasListClient/App.xaml.cs
+++ XmasListClient/App.xaml.cs
@@ -29,12 +29,12 @@ namespace XmasListClient
         // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
         // The Authority is the sign-in URL of the tenant.
         //
-        public const string Tenant = "<TENANT NAME>.onmicrosoft.com";
-        public const string ClientId = "<APPLICATION ID>";
+        public const string Tenant = "TENANT_NAME.onmicrosoft.com";
+        public const string ClientId = "APPLICATION_ID";
         public const string AadInstance = "https://login.microsoftonline.com/{0}";
         public static string Authority = string.Format(CultureInfo.InvariantCulture, AadInstance, Tenant);
 
-        public const string XmasListResourceId = "<APP ID URI>";
+        public const string XmasListResourceId = "APP_ID_URI";
         public const string XmasListBaseAddress = "https://localhost:44321/";
 
         /// <summary>
